dda7d720acb11fc2fa113c4216609770bf15519e
yadavdhiru2009 <yadavdhiru2009@gmail.com>
2023-08-30 08:05:47 +0530

    Color Controlled Of 3 LED using Multi Potentiometer and Raspberry Pi zero 2W

---
+++ 16.Color_contolled_3potentiometer/color_controlled_3potentiometer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+"""
+ Author :Dharmendra Kumar Yadav
+ this code is used controll 3 led using 3 Potentiometer
+"""
+from gpiozero import RGBLED, MCP3008
+
+led = RGBLED(red=2, green=3, blue=4)
+red_pot = MCP3008(channel=0)
+green_pot = MCP3008(channel=1)
+blue_pot = MCP3008(channel=2)
+
+while True:
+    led.red = red_pot.value
+    led.green = green_pot.value
+    led.blue = blue_pot.value
