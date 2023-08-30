31e5f0e3ebef024b073d1b296f7074545c028dc6
yadavdhiru2009 <yadavdhiru2009@gmail.com>
2023-08-30 08:00:17 +0530

    ADC Temperature Sensor Interface with Raspberry Pi zero 2W

---
+++ 14.ADC Temperature/adc_temperature.py
@@ -0,0 +1,16 @@
+"""
+ Author :Dharmendra Kumar Yadav
+ this code is used for take data from MCP3008
+"""
+from gpiozero import MCP3008
+from time import sleep
+
+def convert_temp(gen):
+    for value in gen:
+        yield (value * 3.3 - 0.5) * 100
+
+adc = MCP3008(channel=0)
+
+for temp in convert_temp(adc.values):
+    print('The temperature is', temp, 'C')
+    sleep(1)
