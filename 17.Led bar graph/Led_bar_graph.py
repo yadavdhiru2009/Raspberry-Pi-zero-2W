2fe7539001af2d59bc1a40576122b7ba69a63f46
yadavdhiru2009 <yadavdhiru2009@gmail.com>
2023-08-30 08:06:38 +0530

    LED Bar graph Interface With Raspberry Pi zero 2W

---
+++ 17.Led bar graph/Led_bar_graph.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+"""
+ Author :Dharmendra Kumar Yadav
+ this code is used control the LedBar graph at pin no (5, 6, 13, 19, 26, 20)
+"""
+from gpiozero import LEDBarGraph
+from time import sleep
+from __future__ import division  # required for python 2
+
+graph = LEDBarGraph(5, 6, 13, 19, 26, 20)
+
+graph.value = 1  # (1, 1, 1, 1, 1, 1)
+sleep(1)
+graph.value = 1/2  # (1, 1, 1, 0, 0, 0)
+sleep(1)
+graph.value = -1/2  # (0, 0, 0, 1, 1, 1)
+sleep(1)
+graph.value = 1/4  # (1, 0, 0, 0, 0, 0)
+sleep(1)
+graph.value = -1  # (1, 1, 1, 1, 1, 1)
+sleep(1)
